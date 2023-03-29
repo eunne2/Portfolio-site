8b9392b701694e9f2c89c404a0f7c4dbf2694a43
eunne2 <ehye518@gmail.com>
2023-03-29 19:27:26 +0900

    Footer 완료 → 이메일, 전화, 블로그, 깃허브 링크 연결

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -1,8 +1,41 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faP, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faBloggerB, faGithub } from "@fortawesome/free-brands-svg-icons";
+import styles from "./Footer.module.css";
+
 function Footer() {
   return (
-    <>
-      <div>Footer</div>
-    </>
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.contentTitle}>Contact</div>
+        <div className={styles.contact}>
+          <a href="mailto:ehye518@gmail.com">
+            <div className={styles.circle}>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </div>
+          </a>
+          <a href="tel:010-4056-2497">
+            <div className={styles.circle}>
+              <FontAwesomeIcon icon={faPhone} />
+            </div>
+          </a>
+          <a href="https://blog.naver.com/wweoerkk" target="_blank">
+            <div className={styles.circle}>
+              <FontAwesomeIcon icon={faBloggerB} />
+            </div>
+          </a>
+          <a href="https://github.com/eunne2" target="_blank">
+            <div className={styles.circle}>
+              <FontAwesomeIcon icon={faGithub} />
+            </div>
+          </a>
+        </div>
+        <div className={styles.str}>
+          <span className={styles.str2}>Designed & Developed By</span>
+          &nbsp;LeeEunHye
+        </div>
+      </div>
+    </section>
   );
 }
 
