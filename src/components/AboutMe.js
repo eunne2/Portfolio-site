e546b990363b5b2587cb8a386692204bf3872604
eunne2 <ehye518@gmail.com>
2023-03-29 21:33:50 +0900

    About Me 진행중 → 배경 circle 및 사진 첨부

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,8 +1,44 @@
+import styles from "./AboutMe.module.css";
+
 function AboutMe() {
   return (
-    <>
-      <div>About Me</div>
-    </>
+    <section className={styles.section}>
+      <div className={styles.container}>
+        {/* 배경 circle */}
+        <div className={styles.circle}></div>
+
+        {/* 타이틀 */}
+        <div className={styles.sectionTitle}>
+          <div className={styles.sectionName}>About Me</div>
+          <div>밑주우우우우우울</div>
+        </div>
+
+        {/* 본문 */}
+        <div className={styles.content}>
+          <div className={styles.photoSection}>
+            <div className={styles.photo}></div>
+            <div className={styles.myInfo}>
+              <div>어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구</div>
+              <div>어쩌구</div>
+              <div>어쩌구</div>
+              <div>어쩌구</div>
+              <div>어쩌구</div>
+              <div>어쩌구</div>
+              <div>어쩌구</div>
+              <div>어쩌구</div>
+            </div>
+          </div>
+          <div className={styles.infoSection}>
+            <div>문장문장</div>
+            <div>문장문장</div>
+            <div>문장문장</div>
+            <div>문장문장</div>
+            <div>문장문장</div>
+            <div>문장문장</div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
