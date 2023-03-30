7290f921a245a20c8510f90c885502dd30d8be35
eunne2 <ehye518@gmail.com>
2023-03-30 19:29:38 +0900

    About Me 진행중(2) → 전체적인 디자인 완료(웹/모바일), onMouseEnter 이벤트 실행시 class 추가O but 해당 class css 실행X 검토 필요

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,5 @@
 import styles from "./AboutMe.module.css";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,6 +9,34 @@ function AboutMe() {
   useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
+
+  // 타이핑 애니메이션
+  const txt = "꿈은 현실이 된다.";
+  const [Text, setText] = useState("");
+  const [Count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setText(Text + txt[Count]);
+      setCount(Count + 1);
+    }, 200);
+    if (Count === txt.length) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  });
+
+  // 마우스 오버 이벤트
+  const textAni = useRef();
+  const onMouseEnter = () => {
+    textAni.current.classList.add("textAni");
+    console.log("textAni 클래스 추가");
+  };
+
+  const onMouseLeave = () => {
+    textAni.current.classList.remove("textAni");
+    console.log("textAni 클래스 삭제");
+  };
 
   return (
     <section className={styles.section}>
@@ -26,7 +54,7 @@ function AboutMe() {
         <div className={styles.content}>
           <div className={styles.photoSection}>
             <div className={styles.photo}></div>
-            <div className={styles.myInfo}>
+            <div className={styles.myInfo} data-aos="fade-right">
               <div className={styles.myInfoDiv}>
                 <div className={styles.front}>프론트엔드 개발자</div>
                 <div className={styles.name}>이은혜</div>
@@ -70,13 +98,33 @@ function AboutMe() {
               </div>
             </div>
           </div>
-          <div className={styles.infoSection}>
-            <div>문장문장</div>
-            <div>문장문장</div>
-            <div>문장문장</div>
-            <div>문장문장</div>
-            <div>문장문장</div>
-            <div>문장문장</div>
+          <div
+            className={styles.infoSection}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            {/* 타이핑 효과 */}
+            <div className={styles.Text} data-aos="fade-down">
+              {Text}
+              <span className={styles.cursor}></span>
+            </div>
+            <div className={styles.infoBox}>
+              <div className={styles.info} data-aos="fade-down">
+                안녕하세요, 신입 프론트엔드 개발자{" "}
+                <span style={{ fontWeight: "600" }}>이은혜</span>
+                입니다.
+              </div>
+              <div className={styles.info} data-aos="fade-down">
+                <span ref={textAni}>상상한 아이디어를 직접 구현하는 것</span>이
+                좋습니다. 주변 환경에 빠르게 적응하고, 새로운 기술을 배우며
+                회사와 함께 성장하고 싶습니다.
+              </div>
+              <div className={styles.info} data-aos="fade-down">
+                노력없이 쓰인 글은 대개 감흥 없이 읽힌다고 합니다. 꾸준히 배우고
+                노력하고 개선하여 사용자에게 깊은 여운을 남기는 프론트엔드
+                개발자가 되고 싶습니다.
+              </div>
+            </div>
           </div>
         </div>
       </div>
